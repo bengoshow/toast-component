dee383172b884d0cfc23dbbf0b743221f06cd3c2
Ben Goshow <goshow@heysparkbox.com>
2023-04-26 13:13:36 -0400

    feat: update component with state values
    EXERCISE-2

--- src/components/ToastPlayground/ToastPlayground.js
+++ src/components/ToastPlayground/ToastPlayground.js
@@ -1,6 +1,8 @@
 import React from 'react';
 
 import Button from '../Button';
+import Toast from '../Toast'
+import useToggle from '../../hooks/useToggle';
 
 import styles from './ToastPlayground.module.css';
 
@@ -9,12 +11,15 @@ const VARIANT_OPTIONS = ['notice', 'warning', 'success', 'error'];
 function ToastPlayground() {
   const [message, setMessage] = React.useState('');
   const [variant, setVariant] = React.useState(VARIANT_OPTIONS[0]);
+  const [isVisible, toggleIsVisible] = useToggle();
   return (
     <div className={styles.wrapper}>
       <header>
         <img alt="Cute toast mascot" src="/toast.png" />
         <h1>Toast Playground</h1>
       </header>
+
+      {isVisible && <Toast {...{ variant, message, toggleIsVisible }} />}
 
       <div className={styles.controlsWrapper}>
         <div className={styles.row}>
@@ -66,7 +71,7 @@ function ToastPlayground() {
           <div
             className={`${styles.inputWrapper} ${styles.radioWrapper}`}
           >
-            <Button>Pop Toast!</Button>
+            <Button onClick={toggleIsVisible}>Pop Toast!</Button>
           </div>
         </div>
       </div>
