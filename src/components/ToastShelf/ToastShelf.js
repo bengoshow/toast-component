d268c839728d574af89127cd57c2f325790f09d1
Ben Goshow <goshow@heysparkbox.com>
2023-04-27 08:22:32 -0400

    feat: adds screen-reader support for Toasts
    EXERCISE-5

--- src/components/ToastShelf/ToastShelf.js
+++ src/components/ToastShelf/ToastShelf.js
@@ -8,7 +8,12 @@ import { ToastsContext } from '../ToastProvider/ToastProvider';
 function ToastShelf() {
   const { toasts } = React.useContext(ToastsContext);
   return (
-    <ol className={styles.wrapper}>
+    <ol
+      className={styles.wrapper}
+      role="region"
+      aria-live="polite"
+      aria-label="Notification"
+    >
       {toasts.map(({ id, variant, message }) => {
         return (
           <li key={id} className={styles.toastWrapper}>
